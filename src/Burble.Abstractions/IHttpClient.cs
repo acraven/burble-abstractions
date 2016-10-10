3bd2430b0303c2d31af0d76bb9f667c82b5e6aa9
acraven <andrew@epworth-consulting.co.uk>
2016-10-10 19:50:14 +0100

    Initial commit

---
+++ src/Burble.Abstractions/IHttpClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Burble.Abstractions
+{
+   using System.Net.Http;
+   using System.Threading.Tasks;
+
+   public interface IHttpClient
+   {
+      Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+   }
+}
---
+++ src/Burble.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Burble.Abstractions")]
+[assembly: AssemblyDescription("HTTP API library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Epworth Consulting Ltd.")]
+[assembly: AssemblyProduct("Burble")]
+[assembly: AssemblyCopyright("© Andrew Craven 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
