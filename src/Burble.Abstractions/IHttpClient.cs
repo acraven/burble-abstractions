2960c5af9d6d74255d9d07b9fb6b5b6020f70d5c
acraven <andrew@epworth-consulting.co.uk>
2016-11-15 12:34:17 +0000

    Add base address to http client interface

--- src/Burble.Abstractions/IHttpClient.cs
+++ src/Burble.Abstractions/IHttpClient.cs
@@ -1,10 +1,13 @@
 ﻿namespace Burble.Abstractions
 {
+   using System;
    using System.Net.Http;
    using System.Threading.Tasks;
 
    public interface IHttpClient
    {
+      Uri BaseAddress { get; }
+
       Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
    }
 }
